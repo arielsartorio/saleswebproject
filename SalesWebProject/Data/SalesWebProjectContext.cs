538a0531bf3dd843baadf2b793544908a19ecb23
Ariel Sartorio <ariel.sartorio@live.com>
2023-03-17 17:55:11 -0300

    Criação do projeto e do CRUD do Department

---
+++ SalesWebProject/Data/SalesWebProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebProject.Models;
+
+namespace SalesWebProject.Data
+{
+    public class SalesWebProjectContext : DbContext
+    {
+        public SalesWebProjectContext (DbContextOptions<SalesWebProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebProject.Models.Department> Department { get; set; }
+    }
+}
---
+++ SalesWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
