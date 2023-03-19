1cbc37a60613e1fbca711a9b3fc29a782d856fa9
Ariel Sartorio <ariel.sartorio@live.com>
2023-03-18 23:27:30 -0300

    SellerService e FindAll básico

---
+++ SalesWebProject/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using SalesWebProject.Data;
+using System.Collections.Generic;
+using System.Linq;
+using SalesWebProject.Models;
+
+namespace SalesWebProject.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebProjectContext _context;
+
+        public SellerService(SalesWebProjectContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SalesWebProject/Startup.cs
+++ SalesWebProject/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebProject.Data;
+using SalesWebProject.Services;
 
 namespace SalesWebProject
 {
@@ -40,6 +41,7 @@ namespace SalesWebProject
                     options.UseMySql(Configuration.GetConnectionString("SalesWebProjectContext"), builder => builder.MigrationsAssembly("SalesWebProject")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
